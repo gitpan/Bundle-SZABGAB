b1d61b7401dad973bd0c408e4418e0b2a8209141
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2006-10-31 08:25:39 +0000

    Import of SZABGAB/Bundle-SZABGAB-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-SZABGAB
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SZABGAB/Bundle-SZABGAB-0.01.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/SZABGAB.pm
@@ -0,0 +1,37 @@
+package Bundle::SZABGAB;
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::SZABGAB - Bundle of some "stable" version of modules
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::SZABGAB'
+
+=head1 CONTENTS
+
+M/MS/MSERGEANT/DBD-SQLite-1.12.tar.gz
+
+=head1 DESCRIPTION
+
+This Bundle includes particular version of some CPAN modules that
+seem to work together well.
+
+=head1 CHANGES
+
+0.01 2006.10.31
+
+    First test version to see if I know how to use the version freezing
+    feature
+
+=head1 AUTHOR
+
+Gabor Szabo
+
